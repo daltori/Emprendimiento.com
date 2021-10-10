681a7dbc38557278714920643116875faa656cf9
daltori <davidalejandrotorresrivera1998@hotmail.com>
2021-10-09 22:41:49 -0500

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <carrucel></carrucel>
+    
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Carrucel from '../components/Carrucel.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Carrucel
+  }
+}
+</script>
